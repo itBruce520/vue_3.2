d70551334c885dba0256032b07aa0684213e3d97
itBruce <93927430@qq.com>
2022-05-09 20:08:21 +0800

    feat: layout

--- vue.config.js
+++ vue.config.js
@@ -84,5 +84,16 @@ module.exports = {
         }
       }
     }
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        // 8版本用prependData:
+        additionalData: `
+          @import "@/styles/variables.scss";  // scss文件地址
+          @import "@/styles/mixin.scss";     // scss文件地址
+        `
+      }
+    }
   }
 }
